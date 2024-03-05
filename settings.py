292695b749c931b1f3119b98d40af50fdbb3df02
Azibator Banigo <azibatorb@gmail.com>
2024-03-05 16:14:08 +0100

    Added fleet

--- settings.py
+++ settings.py
@@ -4,8 +4,8 @@ class Settings:
     def __init__(self):
         """ Initialize the game's settings """
         #Screen settings
-        self.screen_width = 800
-        self.screen_height = 400
+        self.screen_width = 1200
+        self.screen_height = 800
         self.bg_color = (255, 255, 238)
         
         # Ship Settings
